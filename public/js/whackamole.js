2530cd7b1298a4869be10ab7622ec19d648a0159
Calvin Glover <calvinglover@Calvins-MacBook-Air.local>
2015-01-14 09:59:08 -0600

    whackamole game pre-jQuery countdown plugin

--- public/js/whackamole.js
+++ public/js/whackamole.js
@@ -1,22 +1,4 @@
-// Returns a random integer between 0 (included) and 9 (excluded)
-function getRandomInt() {
-  return Math.floor(Math.random() * (9 - 0)) + 0;
-}
-
-
-function clickBox() {
-	$('.emptyHole').on('click',(function() {
-    	$(this).addClass('activeHole');
-    }));
-    console.log('running');
-}
-
-function stopClicking() {
-	clearInterval(interval);
-	// Remove all existing event listeners from the boxes
-	$('.emptyHole').off();
-
-}
+// Define Variables
 
 // Generate A Random Number
  var random = getRandomInt();
@@ -24,13 +6,32 @@ function stopClicking() {
 // Define an array of boxes
  var game = $('#gameBox').children();
 
- // Use the number to pick one random box
+ var whackTime = null;
 
 
- var interval = null;
+// Define Functions
+// Returns a random integer between 0 (included) and 9 (excluded)
+function getRandomInt() {
+  return Math.floor(Math.random() * (9 - 0)) + 0;
+}
 
+function stopClicking() {
+	console.log(whackTime);
+	clearInterval(whackTime);
+	$('.emptyHole').off();
+	// console.log('Calvin Rocks!');
+}
+
+function moveBox() {
+	var trandom = getRandomInt();
+	$(game).removeClass('activeHole');
+    $(game[trandom]).addClass('activeHole');
+
+}
+
+// Event Listeners
 $('#startButton').click(function(){
-	interval = setInterval(clickBox,1000);
+	whackTime = setInterval(moveBox, 1000);
 });
 
 $('#stopButton').click(stopClicking);
@@ -39,26 +40,13 @@ $('#stopButton').click(stopClicking);
 
 
 
-// $('#button').click(function(){
-//     $(game[random]).addClass('activeHole');   
-//     });
-
-// $('#button').click(function(){
-// 	// interval = setInterval(testFunction,1000);
-// 	interval = setInterval(clickBox,1000);
-
-//     });
 
 
 
 
 
-
-// function testFunction() {
-// 	var calvin = 'Calvin is great!';
-// 	console.log(calvin);
-// }
-
-
-// Grabs a random emptyHole and adds class activeHole
-// $(game[random]).addClass('activeHole');
+// setInterval(function(){
+// 	var trandom = getRandomInt();
+// 	$(game).removeClass('activeHole');
+//     $(game[trandom]).addClass('activeHole');
+// }, 1000);
