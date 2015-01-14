155cf323ad5bb9493a7c82d3b2cd48d3dae3ba3e
Calvin Glover <calvinglover@Calvins-MacBook-Air.local>
2015-01-14 15:02:51 -0600

    whackamole with working counter, score keeping

--- public/js/whackamole.js
+++ public/js/whackamole.js
@@ -1,52 +1,81 @@
 // Define Variables
 
 // Generate A Random Number
- var random = getRandomInt();
+ var random;
 
-// Define an array of boxes
+// // Define an array of boxes
  var game = $('#gameBox').children();
 
  var whackTime = null;
 
+ var score = null;
+
+
 
 // Define Functions
+
 // Returns a random integer between 0 (included) and 9 (excluded)
 function getRandomInt() {
-  return Math.floor(Math.random() * (9 - 0)) + 0;
+  return Math.floor(Math.random() * (8 - 0)) + 0;
 }
 
 function stopClicking() {
-	console.log(whackTime);
 	clearInterval(whackTime);
 	$('.emptyHole').off();
-	// console.log('Calvin Rocks!');
+}
+
+var timeLeft = 5;
+var timerId;
+
+function timer() {
+	timerID = setInterval(countdown, 1000);
+}
+
+function countdown() {
+  if (timeLeft == -1) {
+    clearInterval(timerId);
+    $('#startButton').off();
+    console.log('Game Over!');
+    console.log(timeLeft);
+  } else {
+    $('#shotClock span').html(timeLeft);
+    timeLeft--;
+  }
 }
 
 function moveBox() {
-	var trandom = getRandomInt();
+	random = getRandomInt();
 	$(game).removeClass('activeHole');
-    $(game[trandom]).addClass('activeHole');
+    $(game[random]).addClass('activeHole');
+    $(game[random]).on('click',keepScore);
 
 }
 
+function keepScore() {
+	score = score +1;
+	$('#myScore span').html(score);
+}
+
+
+
+
+
+
+
+
+
+
 // Event Listeners
-$('#startButton').click(function(){
-	whackTime = setInterval(moveBox, 1000);
-});
 
-$('#stopButton').click(stopClicking);
+$('#startButton').on('click', timer);
 
+// $('#startButton').click(function(){
+// 	whackTime = setInterval(moveBox, 1000);
+// });
 
+// $('#stopButton').click(stopClicking);
 
-
-
-
-
+// $('#increment').on('click',shotClock);
 
 
 
-// setInterval(function(){
-// 	var trandom = getRandomInt();
-// 	$(game).removeClass('activeHole');
-//     $(game[trandom]).addClass('activeHole');
-// }, 1000);
